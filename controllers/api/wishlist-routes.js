667d4934ff5c67b61f74a6f21a6d9ba0ba9e50ae
Sean Craig <seancraig21@gmail.com>
2021-11-11 15:22:39 -0600

    add games to library and wishlist working

--- controllers/api/wishlist-routes.js
+++ controllers/api/wishlist-routes.js
@@ -1,5 +1,6 @@
 const router = require("express").Router();
 const { Wishlist, Game, GameWishlist } = require("../../models");
+const withAuth = require("../../utils/auth");
 
 // router.get("/me", async (req, res) => {
 //   try {
@@ -26,5 +27,21 @@ const { Wishlist, Game, GameWishlist } = require("../../models");
 //     res.status(500).json(err);
 //   }
 // });
+
+router.post("/", withAuth, async (req, res) => {
+  try {
+    const wishlist = await Wishlist.findOne({
+      where: {
+        user_id: req.session.user_id,
+      },
+    });
+    const game = await Game.findOne({ where:{id: req.body.game_id}});
+    const newGame = await wishlist.addGame(game);
+    res.status(200).json(newGame);
+  } catch (err) {
+    console.log(err);
+    res.status(500).json(err);
+  }
+});
 
 module.exports = router;
--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -2,23 +2,6 @@ const router = require('express').Router();
 const { User, Library, Game, GameLibrary } = require('../models');
 const withAuth = require('../utils/auth');
 
-// router.get('/', withAuth, async (req, res) => {
-//   try {
-//     const userData = await User.findAll({
-//       attributes: { exclude: ['password'] },
-//       order: [['name', 'ASC']],
-//     });
-
-//     const users = userData.map((project) => project.get({ plain: true }));
-
-//     res.render('homepage', {
-//       users,
-//       logged_in: req.session.logged_in,
-//     });
-//   } catch (err) {
-//     res.status(500).json(err);
-//   }
-// });
 router.get("/id/:id", async (req, res) => {
   try {
     const dbGameData = await Game.findOne({
@@ -46,7 +29,7 @@ router.get("/", (req, res) => {
     res.render("homepage");
 })
 
-router.get("/me", async (req, res) => {
+router.get("/me", withAuth, async (req, res) => {
   try {
     const dbLibraryData = await Library.findOne({
       where: {
