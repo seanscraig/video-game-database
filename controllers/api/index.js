aaaa75d6963746028ba33d84ed9edadfaae1834c
Sean Craig <seancraig21@gmail.com>
2021-11-11 10:49:43 -0600

    library get route is working

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -1,8 +1,10 @@
 const router = require('express').Router();
 const userRoutes = require('./user-routes');
 const gameRoutes = require('./game-routes');
+const libraryRoutes = require('./library-routes');
 
 router.use('/users', userRoutes);
 router.use('/games', gameRoutes);
+router.use('/librarys', libraryRoutes);
 
 module.exports = router;
---
+++ controllers/api/library-routes.js
@@ -0,0 +1,34 @@
+const router = require("express").Router();
+const { Library, Game, GameLibrary, Genre } = require("../../models");
+
+router.get("/me", async (req, res) => {
+  try {
+    const dbLibraryData = await Library.findOne({
+      where: {
+        user_id: req.session.user_id,
+      },
+      attributes: ["id", "is_public", "user_id"],
+      include: {
+        model: Game,
+        through: GameLibrary,
+        // as: "game_in_library",
+        // include: {
+        //   model: Game,
+        // },
+      },
+    });
+
+    if (!dbLibraryData) {
+      res.status(404).json({ message: "No library found with this id." });
+      return;
+    }
+
+    const library = dbLibraryData.get({ plain: true });
+    res.status(200).json(library);
+  } catch (err) {
+    console.log(err);
+    res.status(500).json(err);
+  }
+});
+
+module.exports = router;
